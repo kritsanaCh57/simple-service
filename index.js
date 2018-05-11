436e840b062ad73d76aa8b31fbb02ab00426943f
kritsanach57 <kritsana.ch.57@ubu.ac.th>
2018-05-11 15:50:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var bodyParser  = require('body-parser');
+var cors = require('cors');
+var POST = process.env.PORT || 3000;
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+}));
+app.use(cors());
+
+app.get('/', function (req, res, next) {
+  res.send("This is index 1."+ POST );
+}); 
+
+
+var server = app.listen(POST, function(){
+  console.log('express is running on port:'+POST);
+});
